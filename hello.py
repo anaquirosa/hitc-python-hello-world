3adb5813912d6b7c59443ff3ffa95c050f29c430
Mili Sucevic <93227818+mili-sucevic@users.noreply.github.com>
2023-09-01 10:00:36 +0200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,21 @@
+import random
+
+# List of fun greeting messages
+greetings = [
+    "Hello, world!",
+    "Hi there!",
+    "Howdy!",
+    "Hey!",
+    "Yo!",
+    "Greetings, Earthling!",
+    "Hola!",
+    "Bonjour!",
+    "Ciao!",
+    "Namaste!",
+]
+
+# Select a random greeting from the list
+random_greeting = random.choice(greetings)
+
+# Print the random greeting
+print(random_greeting)
